b6595687d6f58fbc2228831e352a44db94f7f46b
joao911 <joaopaulolacerda911@gmail.com>
2020-07-27 21:28:47 -0300

    componentes header balance e income expensive parcialmente feitos

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <>
+          <h1>Your balance</h1>  
+          <h4>R$0.00</h4>
+        </>
+    )
+}
+
+export default Balance
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Ratreador de Despesas</h2>
+        </div>
+    )
+}
+
+export default Header
